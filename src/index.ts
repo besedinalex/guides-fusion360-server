373357ee950b6fcc0f1c741f3e9bef540cc53d79
Alexey <besedinalexey@gmail.com>
2019-10-30 21:13:38 +0300

    Added cors

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import express from 'express';
+import cors from 'cors';
 
 export const app = express();
 
+app.use(cors());
 app.listen(4000, () => console.log('Server is successfully started!'));
