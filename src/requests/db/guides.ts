5daf8689ad915dab0fee5499aa3849526ccffb2d
Alexey <besedinalexey@gmail.com>
2019-10-31 00:43:11 +0300

    Additional @types

--- src/requests/db/guides.ts
+++ src/requests/db/guides.ts
@@ -4,7 +4,7 @@ import Guide from "../../interfaces/guide";
 export function selectGuides(): Promise<Guide[]> {
     return new Promise((resolve, reject) => {
         const sql = `SELECT * FROM Guides`;
-        db.all(sql, [], (err, rows) => {
+        db.all(sql, [], (err, rows: Guide[]) => {
             if (err) {
                 reject(err);
             } else {
