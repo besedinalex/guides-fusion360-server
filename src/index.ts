93a4dbbb2d1a39f537413a0a266a64e37ce8dabd
Alexey <besedinalexey@gmail.com>
2019-10-30 21:41:32 +0300

    Basic functionality

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,8 @@ export const app = express();
 
 app.use(cors());
 app.listen(4000, () => console.log('Server is successfully started!'));
+
+app.get('/preview', function (req, res) {
+    console.log(req.query);
+    res.json({test: 'test'});
+});
