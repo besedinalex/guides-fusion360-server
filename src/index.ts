48e52268cc6e2400e710183a72c5950f8ad9a3c8
Alexey <besedinalexey@gmail.com>
2019-10-31 13:26:29 +0300

    Models folder access

--- src/index.ts
+++ src/index.ts
@@ -5,21 +5,15 @@ import {getGuides, getPartGuides} from "./requests/http/guides";
 
 const app = express();
 
+// Server settings
+
 app.use(cors());
 app.use('/images', express.static(projectFolderPath() + '/assets/images'));
-
-function projectFolderPath(): string {
-    const folders = __dirname.split('/');
-    folders.splice(folders.length - 1, 1);
-    folders.splice(0, 1);
-    let path = '';
-    for (const folder of folders) {
-        path += '/' + folder;
-    }
-    return path;
-}
+app.use('/models', express.static(projectFolderPath() + '/assets/models'));
 
 app.listen(4000, () => console.log('Server is started.'));
+
+// HTTP requests
 
 app.get('/home', function (req, res) {
     getGuides(res);
@@ -28,3 +22,16 @@ app.get('/home', function (req, res) {
 app.get('/guide', function (req, res) {
     getPartGuides(req.query.guideId, res);
 });
+
+// Additional functions
+
+function projectFolderPath(): string {
+    const path = process.argv[1].split('/');
+    const file = path[path.length - 1];
+    const extension = file.split('.')[1];
+    if (extension === 'ts') {
+        return __dirname.slice(0, __dirname.length - 4);
+    } else {
+        return __dirname.slice(0, __dirname.length - 5);
+    }
+}
