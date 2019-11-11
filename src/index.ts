306aeeeab91fb0deb3b9383f62249513ed9c0637
Alexey <besedinalexey@gmail.com>
2019-11-11 10:45:56 +0300

    Refactoring

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,15 @@
 import express from 'express';
 import cors from 'cors';
-
-import {getGuideImg, getGuides, getPartGuides} from "./requests/http/guides";
+import guide from "./requests/http/guides";
 
 const app = express();
 
-// Server settings
-
 app.use(cors());
+app.use('/guide', guide);
 app.use('/images', express.static(projectFolderPath() + '/assets/images'));
 app.use('/models', express.static(projectFolderPath() + '/assets/models'));
 
 app.listen(4000, () => console.log('Server is started.'));
-
-// HTTP requests
-
-app.get('/home', function (req, res) {
-    getGuides(res);
-});
-
-app.get('/guide-img', function (req, res) {
-    getGuideImg(req.query.guideId, res);
-});
-
-app.get('/guide', function (req, res) {
-    getPartGuides(req.query.guideId, res);
-});
-
-// Additional functions
 
 function projectFolderPath(): string {
     const path = process.argv[1].split('/');
