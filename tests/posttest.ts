58ea61d3cc99ec3eaa194a0b9d669fb8ea3a7fbd
Alexey Besedin <besedinalexey@gmail.com>
2020-08-06 21:21:04 +0300

    Removed useless tests
    Useful ones are still here though

--- tests/posttest.ts
+++
@@ -1,8 +0,0 @@
-import fs from "fs-extra";
-
-const cwd = process.cwd();
-
-if (fs.existsSync(cwd + '/data_temp')) {
-    fs.removeSync(cwd + '/data');
-    fs.renameSync(cwd + '/data_temp', cwd + '/data');
-}
--- tests/pretest.ts
+++
@@ -1,7 +0,0 @@
-import fs from "fs-extra";
-
-const cwd = process.cwd();
-
-if (fs.existsSync(cwd + '/data')) {
-    fs.renameSync(cwd + '/data', cwd + '/data_temp');
-}
