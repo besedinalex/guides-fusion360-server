fbf50b6975e43c9b9785cda2d52d3f23fdf48819
Alexey Besedin <besedinalexey@gmail.com>
2020-09-10 16:33:35 +0300

    Updated automated script for publish

--- README.md
+++ README.md
@@ -35,9 +35,10 @@ Proper way to deploy can be found in [official Microsoft documentation](https://
 ### Easy way:
 
 1. Install Python 3 from [official site](https://www.python.org/downloads/).
-2. Make sure your CLI now handles `python` or `python3` commands.
+2. Make sure your CLI now handles `python3` or `python` command.
 3. Client project folder should be named `/guides-fusion360-client` and placed in the same folder where guides-fusion360-server is. Read it's [README.md](https://github.com/besedinalex/guides-fusion360-client/blob/master/README.md) to see if you need anything else installed and working on your machine.
-4. Run `python build.py`.
-5. Folder with build is `/Fusion360Guide`.
+4. Run `python3 publish.py` or `python publish.py`.
 
-Additional info: use `--os=VALUE` flag to specify OS. Supported values are: `win10` (default), `linux`, `osx`.
+*Additional options:*
+- `--rebuild-client=VALUE` flag specifies whether you need to rebuild React app for `/wwwroot` or not. Supported values are `true` (default) or `false`.
+- `--os=VALUE` flag specifies if you need to build for computer without `dotnet` runtime. Supported values are `linux` or `win10`.
---
+++ publish.py
@@ -0,0 +1,49 @@
+from os import path
+from shutil import rmtree
+from argparse import ArgumentParser
+from subprocess import call
+from distutils.dir_util import copy_tree
+
+parser = ArgumentParser(description='Publish options.')
+parser.add_argument('-o', '--os')
+parser.add_argument('-ic', '--rebuild-client', default='true')
+args = vars(parser.parse_args())
+os = args['os']
+rebuild_client = args['rebuild_client']
+
+if os != None and os != 'linux' and os != 'win10':
+    print('\nSupported --os values are: `linux` and `win10`.\n')
+    quit()
+
+if rebuild_client != 'true' and rebuild_client != 'false':
+    print('\nSupported --rebuild-client values are `true` or `false`.\n')
+    quit()
+
+if rebuild_client == 'true':
+    if not path.isdir('./../guides-fusion360-client'):
+        print('\nFolder where you keep /guides-fusion360-server should contain /guides-fusion360-client.\n')
+        exit()
+
+    if not path.isdir('./../guides-fusion360-client/node_modules'):
+        print('\nGetting React app dependencies.')
+        call('npm ci', shell=True, cwd='../guides-fusion360-client')
+        print('\nFinished getting React app dependencies.')
+
+    print('\nBuilding React app.')
+    call('npm run build', shell=True, cwd='../guides-fusion360-client')
+
+    print('Moving React app build to /wwwroot.')
+    if path.isdir('./GuidesFusion360Server/wwwroot'):
+        rmtree('./GuidesFusion360Server/wwwroot')
+    copy_tree('./../guides-fusion360-client/build', './GuidesFusion360Server/wwwroot')
+
+print('\nBuilding server.\n')
+command = 'dotnet publish -c Release{}'.format('' if os == None else ' -r {}-x64'.format(os))
+call(command, shell=True, cwd='./GuidesFusion360Server')
+
+if path.isdir('./Fusion360Guide'):
+    rmtree('./Fusion360Guide')
+build = './GuidesFusion360Server/bin/Release/netcoreapp3.1/{}publish'.format('' if os == None else '{}-x64/'.format(os))
+copy_tree(build, './Fusion360Guide')
+
+print('\nBuilding is finished and is available in /Fusion360Guide.\n')
